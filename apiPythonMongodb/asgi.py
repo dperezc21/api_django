10af03693b245716e3473a45b06cf6240c89c5ae
dperezc21 <davierperez11.2@gmail.com>
2021-11-26 08:37:18 -0500

    api mongodb

--- apiPythonMongodb/asgi.py
+++ apiPythonMongodb/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_paises_python_mongodb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apiPythonMongodb.settings')
 
 application = get_asgi_application()
--- apiPythonMongodb/settings.py
+++ apiPythonMongodb/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api_paises_python_mongodb',
+    'apiPythonMongodb',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'api_paises_python_mongodb.urls'
+ROOT_URLCONF = 'apiPythonMongodb.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'api_paises_python_mongodb.wsgi.application'
+WSGI_APPLICATION = 'apiPythonMongodb.wsgi.application'
 
 
 # Database
